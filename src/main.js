556d0b233c28446d04b1fa467d2a4a8fe6a8c29f
Viktor Zayats <viktor_zayats@ukr.net>
2026-07-26 21:17:25 +0300

    fix pull

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
 
+// Scroll reveal
 const scrollRevealSelectors = [
   '.section-header-title',
   '.section-header-text',
